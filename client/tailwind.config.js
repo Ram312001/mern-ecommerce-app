48f7328d676559282ef6f442d9633c23b4cf0206
Ram312001 <rampachkawade0101@gmail.com>
2025-02-05 09:47:06 +0530

    Updated Tailwind config file again

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: [
@@ -73,5 +75,5 @@ export default {
       },
     },
   },
-  plugins: [(await import("tailwindcss-animate")).default],
+  plugins: [tailwindcssAnimate],
 };
